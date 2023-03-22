148d580ef5a07a55a196f754250f728ea33baeb0
Big-oh-learn <shubhamingale779@gmail.com>
2023-03-22 11:29:48 +0530

    getDouble method update - Added support for signed numbers in all operations - use: -<operand>

--- cpp/includes/get.cpp
+++ cpp/includes/get.cpp
@@ -14,11 +14,13 @@ double getDouble(string a)
 {
     bool dotFound=false;
     double resultbd = 0, resultad=0, admul=0.1;
+    int negative = 1;
     for(int i=0;i<a.length();i++)
     {
         if(a[i]=='.'){dotFound=true;continue;}
+        if(a[i]=='-'){negative=-1;continue;}
         if(!dotFound){resultbd=resultbd*10+getInt(a[i]);}
         else if(dotFound){resultad=resultad+(getInt(a[i]))*admul;admul*=0.1;}
     }
-    return resultbd+resultad;
+    return (resultbd+resultad)*negative;
 }
